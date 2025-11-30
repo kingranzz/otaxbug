f9f9059de671d0bb3a22e0802386596ebc10599f
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-11-30 22:57:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "7688717854:AAEV53wtuq30LKHLo5iwUaKsXxEIDFG3nYE",//bot tokenlu
+  BOT_TOKEN: "7939910743:AAFkyMkb8zlFtg3i1nK2PTHOX0cl7a-D5xc",//bot tokenlu
   OWNER_ID: ["5965707646"],//id lu
   API_HASH: "",
   API_ID: 0,//ganti 0 dan langsung isi angka nya saja ex: 12323232
